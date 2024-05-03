cfe91b1f865dd7e997959089e29051a9421f467f
Matthias Klein <klein.matthias2001@gmail.com>
2024-05-03 15:32:15 +0200

    fixed Datagen to use Password Encoder

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/datagenerator/UserDataGenerator.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/datagenerator/UserDataGenerator.java
@@ -7,6 +7,7 @@ import at.ac.tuwien.sepr.groupphase.backend.repository.RoleRepository;
 import at.ac.tuwien.sepr.groupphase.backend.repository.UserRepository;
 import at.ac.tuwien.sepr.groupphase.backend.repository.UserRoleRepository;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import jakarta.annotation.PostConstruct;
@@ -18,16 +19,20 @@ public class UserDataGenerator {
     private final UserRoleRepository userRoleRepository;
     private final RoleRepository roleRepository;
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserDataGenerator(UserRoleRepository userRoleRepository, RoleRepository roleRepository, UserRepository userRepository) {
+    public UserDataGenerator(UserRoleRepository userRoleRepository, RoleRepository roleRepository, UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRoleRepository = userRoleRepository;
         this.roleRepository = roleRepository;
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
 
     @PostConstruct
     private void generateMessage() {
+
+
         Role.RoleBuilder rb = new Role.RoleBuilder();
         Role r = rb.withId(1).withroleId("Admin").build();
 
@@ -35,11 +40,10 @@ public class UserDataGenerator {
         UserRole ur = urrb.withroleId(1).withuserId(1).build();
 
         ApplicationUser.ApplicationUserBuilder aub = new ApplicationUser.ApplicationUserBuilder();
-        ApplicationUser au = aub.withemail("admin@email.com").withpassword("password").withid(1).withusername("admin").withhasProfilePicture(false).build();
+        ApplicationUser au = aub.withemail("admin@email.com").withpassword(passwordEncoder.encode("password")).withid(1).withusername("admin").withhasProfilePicture(false).build();
 
         roleRepository.save(r);
         userRepository.save(au);
         userRoleRepository.save(ur);
-
     }
 }
