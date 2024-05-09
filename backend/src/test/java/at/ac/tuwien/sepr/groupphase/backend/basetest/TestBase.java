030892e46ff61de577952c92eec0f20fe952373a
Josef Tungl <e12119040@student.tuwien.ac.at>
2024-05-09 20:04:02 +0200

    #13: add baseTest class which includes the before and after method of the tests

---
+++ backend/src/test/java/at/ac/tuwien/sepr/groupphase/backend/basetest/TestBase.java
@@ -0,0 +1,50 @@
+package at.ac.tuwien.sepr.groupphase.backend.basetest;
+
+import at.ac.tuwien.sepr.groupphase.backend.datagenerator.AllergenDataGenerator;
+import at.ac.tuwien.sepr.groupphase.backend.datagenerator.CategoryDataGenerator;
+import at.ac.tuwien.sepr.groupphase.backend.datagenerator.IngredientDataGenerator;
+import at.ac.tuwien.sepr.groupphase.backend.datagenerator.NutritionDataGenerator;
+import at.ac.tuwien.sepr.groupphase.backend.datagenerator.UserDataGenerator;
+import at.ac.tuwien.sepr.groupphase.backend.repository.RecipeCategoryRepository;
+import at.ac.tuwien.sepr.groupphase.backend.repository.RecipeIngredientRepository;
+import at.ac.tuwien.sepr.groupphase.backend.repository.RecipeRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * This class is used to prepare the data storage before and after each test
+ */
+public abstract class TestBase {
+    @Autowired
+    private AllergenDataGenerator allergenDataGenerator;
+    @Autowired
+    private CategoryDataGenerator categoryDataGenerator;
+    @Autowired
+    private IngredientDataGenerator ingredientDataGenerator;
+    @Autowired
+    private NutritionDataGenerator nutritionDataGenerator;
+    @Autowired
+    private UserDataGenerator userDataGenerator;
+    @Autowired
+    private RecipeRepository recipeRepository;
+    @Autowired
+    private RecipeCategoryRepository recipeCategoryRepository;
+    @Autowired
+    private RecipeIngredientRepository recipeIngredientRepository;
+
+    @BeforeEach
+    public void setupDb() throws Exception {
+        allergenDataGenerator.run();
+        categoryDataGenerator.run();
+        ingredientDataGenerator.run();
+        nutritionDataGenerator.run();
+    }
+
+    @AfterEach
+    public void tearDownDb() {
+        recipeCategoryRepository.deleteAll();
+        recipeIngredientRepository.deleteAll();
+        recipeRepository.deleteAll();
+    }
+}
