440b33235ae5c25bc318c12897143f440ee08e59
Josef Tungl <e12119040@student.tuwien.ac.at>
2024-05-09 13:43:37 +0200

    #13: reworking and adding new repositories

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/repository/RecipeIngredientRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/repository/RecipeIngredientRepository.java
@@ -1,15 +1,11 @@
 package at.ac.tuwien.sepr.groupphase.backend.repository;
 
-import at.ac.tuwien.sepr.groupphase.backend.entity.Nutrition;
+import at.ac.tuwien.sepr.groupphase.backend.entity.RecipeIngredient;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 @DynamicInsert
 @DynamicUpdate
-@Repository
-public interface NutritionRepository extends JpaRepository<Nutrition, Long> {
-    Nutrition findByName(String name);
-
+public interface RecipeIngredientRepository extends JpaRepository<RecipeIngredient, Long> {
 }
--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/repository/RecipeRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/repository/RecipeRepository.java
@@ -1,4 +1,16 @@
 package at.ac.tuwien.sepr.groupphase.backend.repository;
 
-public class RecipeRepository {
+import at.ac.tuwien.sepr.groupphase.backend.entity.Recipe;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@DynamicInsert
+@DynamicUpdate
+@Repository
+public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+    Optional<Recipe> getRecipeById(long id);
 }
