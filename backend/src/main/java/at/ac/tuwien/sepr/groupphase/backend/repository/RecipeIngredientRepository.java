2a631fc043d58922fea4b8d173699116a409390d
Josef Tungl <e12119040@student.tuwien.ac.at>
2024-05-10 19:28:19 +0200

    #13: rework the repositories to match the new entities

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/repository/RecipeIngredientRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/repository/RecipeIngredientRepository.java
@@ -5,7 +5,10 @@ import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 @DynamicInsert
 @DynamicUpdate
 public interface RecipeIngredientRepository extends JpaRepository<RecipeIngredient, Long> {
+    List<RecipeIngredient> getRecipeIngredientsByRecipeId(long id);
 }
--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/repository/RecipeRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/repository/RecipeRepository.java
@@ -1,9 +1,13 @@
 package at.ac.tuwien.sepr.groupphase.backend.repository;
 
 import at.ac.tuwien.sepr.groupphase.backend.entity.Recipe;
+import jakarta.persistence.JoinTable;
+import jakarta.transaction.Transactional;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -12,5 +16,5 @@ import java.util.Optional;
 @DynamicUpdate
 @Repository
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
-    Optional<Recipe> getRecipeById(long id);
+    Optional<Recipe> getRecipeById(@Param("id") long id);
 }
