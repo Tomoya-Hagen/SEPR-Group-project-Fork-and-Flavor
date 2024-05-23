c329b5f796f8008db5592ffc565fac426aaad512
Matthias Klein <klein.matthias2001@gmail.com>
2024-05-23 09:38:54 +0200

    Test and chnages for tests

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/dto/DetailedRecipeDto.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/dto/DetailedRecipeDto.java
@@ -8,6 +8,15 @@ public class DetailedRecipeDto {
     private String name;
 
     private String description;
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
