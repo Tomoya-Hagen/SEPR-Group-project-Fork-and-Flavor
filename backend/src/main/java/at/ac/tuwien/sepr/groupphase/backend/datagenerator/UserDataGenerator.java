8d9df496b558f65b633facea3cab7d59bba035e3
Pawel Kusiak <e12223334@student.tuwien.ac.at>
2024-05-27 13:07:08 +0200

    [25] fixed issue where Message Entity and Repository where deleted, but not MessageDataGenerator.java.

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/datagenerator/UserDataGenerator.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/datagenerator/UserDataGenerator.java
@@ -5,16 +5,13 @@ import at.ac.tuwien.sepr.groupphase.backend.entity.Role;
 import at.ac.tuwien.sepr.groupphase.backend.repository.RoleRepository;
 import at.ac.tuwien.sepr.groupphase.backend.repository.UserRepository;
 import jakarta.annotation.PostConstruct;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
-import jakarta.annotation.PostConstruct;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
-@Order(1)
 public class UserDataGenerator {
 
     // Autowired dependencies
