5e651bfec7c3689cd258ac6c0d17ba81f57265f0
Martin-Paul Patscheider <e12119828@student.tuwien.ac.at>
2024-06-18 15:07:48 +0200

    added notifier

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/impl/RatingServiceImpl.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/service/impl/RatingServiceImpl.java
@@ -11,6 +11,7 @@ import at.ac.tuwien.sepr.groupphase.backend.exception.NotFoundException;
 import at.ac.tuwien.sepr.groupphase.backend.exception.ValidationException;
 import at.ac.tuwien.sepr.groupphase.backend.repository.RatingRepository;
 import at.ac.tuwien.sepr.groupphase.backend.repository.RecipeRepository;
+import at.ac.tuwien.sepr.groupphase.backend.service.EmailService;
 import at.ac.tuwien.sepr.groupphase.backend.service.RatingService;
 import at.ac.tuwien.sepr.groupphase.backend.service.UserManager;
 import at.ac.tuwien.sepr.groupphase.backend.service.validators.RatingValidator;
@@ -32,17 +33,19 @@ public class RatingServiceImpl implements RatingService {
     private final RatingMapper ratingMapper;
     private final RatingValidator ratingValidator;
     private final UserManager userManager;
+    private final EmailService emailService;
 
     public RatingServiceImpl(RecipeRepository recipeRepository,
                              RatingRepository ratingRepository,
                              RatingMapper ratingMapper,
                              RatingValidator ratingValidator,
-                             UserManager userManager) {
+                             UserManager userManager, EmailService emailService) {
         this.recipeRepository = recipeRepository;
         this.ratingRepository = ratingRepository;
         this.ratingMapper = ratingMapper;
         this.ratingValidator = ratingValidator;
         this.userManager = userManager;
+        this.emailService = emailService;
     }
 
     @Override
@@ -69,6 +72,10 @@ public class RatingServiceImpl implements RatingService {
         rating.setRecipe(recipe);
         rating.setUser(user);
         ratingRepository.save(rating);
+
+        emailService.sendSimpleEmail(recipe.getOwner().getEmail(), "Neue Bewertung!", "Neue Bewertung für das Rezept " + recipe.getName() + " erhalten.\n\n"
+            + rating.toEmailString() + "\n");
+
         return ratingMapper.mapRatingToRatingListDto(rating);
     }
 }
