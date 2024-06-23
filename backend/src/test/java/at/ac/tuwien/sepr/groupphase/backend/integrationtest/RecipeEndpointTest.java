18f8a35beeab49c4bdf5ebe9c9c124d36c2050d9
Matthias Klein <klein.matthias2001@gmail.com>
2024-06-23 19:49:29 +0200

    fixed test

--- backend/src/test/java/at/ac/tuwien/sepr/groupphase/backend/integrationtest/RecipeEndpointTest.java
+++ backend/src/test/java/at/ac/tuwien/sepr/groupphase/backend/integrationtest/RecipeEndpointTest.java
@@ -293,17 +293,15 @@ class RecipeEndpointTest implements TestData {
             "Category 1000 not found",
             "Ingredient 10000 not found",
             "Step Step eins is not valid",
-            "Step Step zwei is not valid",
+            "Step Step zwei not found",
             "Step Step drei is not valid",
-            "Step Step vier is not valid"
+            "Step Step vier not found"
         };
 
         // Check each required line
-
-        System.out.println(response);
+        var lines = response.getContentAsString();;
         for (String requiredLine : requiredLines) {
-            System.out.println(requiredLine);
-            assertTrue(response.getContentAsString().contains(requiredLine));
+            assertTrue(response.getContentAsString().contains(requiredLine),requiredLine);
         }
     }
 
