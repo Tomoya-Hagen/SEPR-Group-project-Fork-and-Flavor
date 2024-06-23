0e27b1f8581a709c8bf6d1bcd531f0bc6058b26c
Josef Tungl <e12119040@student.tuwien.ac.at>
2024-06-23 18:13:00 +0200

    #28: create first design of the frontend

--- frontend/src/app/components/weekplan/weekplan-create/weekplan-create.component.ts
+++ frontend/src/app/components/weekplan/weekplan-create/weekplan-create.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Form } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { Observable } from 'rxjs/internal/Observable';
 import { RecipeBookListDto } from 'src/app/dtos/recipe-book';
+import { Daytime, WeekDayDto, WeekDayEntity, WeekPlanCreateDto, Weekday } from 'src/app/dtos/weekplan';
 import { RecipeBookService } from 'src/app/services/recipebook.service';
+import { WeekplanService } from 'src/app/services/weekplan.service';
 
 @Component({
   selector: 'app-weekplan-create',
@@ -11,22 +15,119 @@ import { RecipeBookService } from 'src/app/services/recipebook.service';
 })
 export class WeekplanCreateComponent implements OnInit {
   ngOnInit(): void {
-      let observable = this.recipeBookService.getRecipeBooksTheUserHasWriteAccessTo();
-      observable.subscribe({
-        next: data => {
-          this.recipeBooks = data;
-        },
-        error: error => {
-          console.error('Error fetching recipe books', error);
-          this.notification.error('Rezeptbücher können nicht abgerufen werden.', "Backend Fehler - Rezepte");
-          this.router.navigate([''])
-        }
-      });
   }
-  recipeBooks = [];
+  public Daytime = Daytime;
+  fromDate: string | null = null;
+  toDate: string | null = null;
+  createDto: WeekPlanCreateDto = {
+    startDate: null,
+    endDate: null,
+    weekdays: [],
+    recipeBookId: 0
+  };
+  currentRecipeBook: RecipeBookListDto | null = null;
+  dummyRecipeBookSelectionModel: unknown;
+  recipeBookSuggestions = (input: string): Observable<RecipeBookListDto[]> =>
+    this.recipeBookService.getRecipeBooksTheUserHasWriteAccessTo()
+
+  weekdays : WeekDayEntity[]= [
+    {
+      weekday: Weekday.Monday,
+      dayTimes:[]
+    },
+    {
+      weekday: Weekday.Tuesday,
+      dayTimes:[]
+    },
+    {
+      weekday: Weekday.Wednesday,
+      dayTimes:[]
+    },
+    {
+      weekday: Weekday.Thursday,
+      dayTimes:[]
+    },
+    {
+      weekday: Weekday.Friday,
+      dayTimes:[]
+    },
+    {
+      weekday: Weekday.Saturday,
+      dayTimes:[]
+    },
+    {
+      weekday: Weekday.Sunday,
+      dayTimes:[]
+    }
+  ];
+
+  selectedRecipeBook:RecipeBookListDto;
 
   constructor(
     private recipeBookService: RecipeBookService,
+    private weekplanService: WeekplanService,
     private router: Router,
     private notification: ToastrService,){}
+
+  onSubmit(form:Form){
+    if(!this.isFormValid()){
+      return;
+    }
+    this.createDto.startDate = new Date(this.fromDate);
+    this.createDto.endDate = new Date(this.toDate);
+    for (let i = 0; i < this.weekdays.length; i++) {
+      let currentWeekday : WeekDayDto = {weekday: "", dayTimes: []};
+      currentWeekday.weekday = Object.keys(Weekday).filter((x) => Weekday[x] == this.weekdays[i].weekday)[0];
+      for (let j = 0; j < this.weekdays[i].dayTimes.length; j++) {        
+        currentWeekday.dayTimes.push(Object.keys(Daytime).filter((x) => Daytime[x] == this.weekdays[i].dayTimes[j])[0]);
+      }
+      this.createDto.weekdays.push(currentWeekday);
+    }
+    this.createDto.recipeBookId = this.currentRecipeBook.id;
+    
+    this.weekplanService.createWeekplan(this.createDto).subscribe({
+      next: (data: any) => {
+      },
+      error: error => {
+        {
+          console.error('Error creating weekplan.', error);
+          this.notification.error('Wochenplan könnte nicht erstellt werden.', 'Backend Fehler - Wochenplan');
+        }
+      }
+    }
+  );
+  }
+
+  public formatRecipeBook(recipeBook: RecipeBookListDto | null): string {
+    return !recipeBook
+      ? ""
+      : `${recipeBook.name}`
+  }
+
+  onCheckboxChange(event:any, day:WeekDayEntity, daytime: Daytime){
+    if(event.target.checked){
+      this.weekdays[this.weekdays.indexOf(day)].dayTimes.push(daytime);
+    } else {
+      this.weekdays[this.weekdays.indexOf(day)].dayTimes.splice(
+      this.weekdays[this.weekdays.indexOf(day)].dayTimes.indexOf(daytime),1);
+    }    
+  }
+
+  public selectRecipeBook(recipeBook: RecipeBookListDto | null) {
+    this.currentRecipeBook = recipeBook;
+  }
+
+  isFormValid():boolean{
+    if(this.currentRecipeBook === null){
+        console.error('Form invalid');
+        this.notification.error('Es muss ein Rezeptbuch ausgewählt werden.');
+        return false;
+    }
+    if(this.fromDate === null || this.toDate === null){
+      console.error('Form invalid');
+        this.notification.error('Es müssen Start- und Enddatum ausgewählt worden sein.');
+        return false;
+    }
+    return true;
+  }
 }
