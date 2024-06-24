e03e0d232a90e013fbc23f7ae51bb5cd8fddb181
Martin-Paul Patscheider <e12119828@student.tuwien.ac.at>
2024-06-25 00:44:06 +0200

    wochenplan erstellbar boolean und Fehlermeldung

--- backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/dto/RecipeBookDetailDto.java
+++ backend/src/main/java/at/ac/tuwien/sepr/groupphase/backend/endpoint/dto/RecipeBookDetailDto.java
@@ -5,13 +5,12 @@ import jakarta.validation.constraints.NotNull;
 import java.util.List;
 
 public record RecipeBookDetailDto(
-    @NotNull
-    long id,
-    @NotNull
-    String name,
+    @NotNull long id,
+    @NotNull String name,
     String description,
     long ownerId,
     UserDto owner,
     List<UserListDto> users,
-    List<RecipeListDto> recipes) {
+    List<RecipeListDto> recipes,
+    boolean canBeMadeWeekPlan) {
 }
